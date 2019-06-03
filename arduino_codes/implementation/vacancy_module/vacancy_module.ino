e8a37e9959a37b7dc19b07ad769d782d7b60793f
unknown <gaborzsolt2007@yahoo.com>
2019-06-03 20:57:25 +0300

    Simplified code to test vacancy_module.ino

--- arduino_codes/implementation/vacancy_module/vacancy_module.ino
+++ arduino_codes/implementation/vacancy_module/vacancy_module.ino
@@ -1,16 +1,13 @@
-const int ledPinSource = 10;
 const int ledPinStatus = 8;
 const int photorPin = A0;
 
 void setup(){
   Serial.begin(9600);
-  pinMode(ledPinSource, OUTPUT);
   pinMode(ledPinStatus, OUTPUT);
   pinMode(photorPin, INPUT);
 }
 
 void loop() {
-  digitalWrite(ledPinSource, HIGH);
   int ldrStatus = analogRead(photorPin);
   if (ldrStatus <= 300) {
     digitalWrite(ledPinStatus, LOW);
